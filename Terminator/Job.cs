282aaa223d2b3bfe2b73fccdb179c58045eac489
Edu <eduardomk@outlook.com>
2020-10-27 11:10:30 -0300

    Terminate multiple windows process faster using regex patterns

---
+++ Terminator/Job.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Terminator
+{
+    public class Job : IDisposable
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr CreateJobObject(IntPtr a, string lpName);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetInformationJobObject(IntPtr hJob, JobObjectInfoType infoType, IntPtr lpJobObjectInfo, uint cbJobObjectInfoLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AssignProcessToJobObject(IntPtr job, IntPtr process);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        private IntPtr _handle;
+        private bool _disposed;
+
+        public Job()
+        {
+            _handle = CreateJobObject(IntPtr.Zero, null);
+
+            var info = new JobobjectBasicLimitInformation
+            {
+                LimitFlags = (int)LimitFlags.JobObjectLimitKillOnJobClose,
+            };
+
+            var extendedInfo = new JobobjectExtendedLimitInformation
+            {
+                BasicLimitInformation = info
+            };
+
+            var length = Marshal.SizeOf(typeof(JobobjectExtendedLimitInformation));
+            var extendedInfoPtr = Marshal.AllocHGlobal(length);
+            Marshal.StructureToPtr(extendedInfo, extendedInfoPtr, false);
+
+            if (!SetInformationJobObject(_handle, JobObjectInfoType.ExtendedLimitInformation, extendedInfoPtr, (uint)length))
+                throw new Exception($"Unable to set information.  Error: {Marshal.GetLastWin32Error()}");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing) { }
+
+            Close();
+            _disposed = true;
+        }
+
+        public void Close()
+        {
+            CloseHandle(_handle);
+            _handle = IntPtr.Zero;
+        }
+
+        public bool AddProcess(IntPtr processHandle)
+        {
+            return AssignProcessToJobObject(_handle, processHandle);
+        }
+
+        public bool AddProcess(int processId)
+        {
+            return AddProcess(Process.GetProcessById(processId).Handle);
+        }
+
+        internal void AddProcess(IEnumerable<IntPtr> processHandles)
+        {
+            foreach (var processHandle in processHandles)
+                AssignProcessToJobObject(_handle, processHandle);
+        }
+    }
+
+    #region Helper classes
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct IoCounters
+    {
+        public ulong ReadOperationCount;
+        public ulong WriteOperationCount;
+        public ulong OtherOperationCount;
+        public ulong ReadTransferCount;
+        public ulong WriteTransferCount;
+        public ulong OtherTransferCount;
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct JobobjectBasicLimitInformation
+    {
+        public long PerProcessUserTimeLimit;
+        public long PerJobUserTimeLimit;
+        public uint LimitFlags;
+        public UIntPtr MinimumWorkingSetSize;
+        public UIntPtr MaximumWorkingSetSize;
+        public uint ActiveProcessLimit;
+        public UIntPtr Affinity;
+        public uint PriorityClass;
+        public uint SchedulingClass;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SecurityAttributes
+    {
+        public uint nLength;
+        public IntPtr lpSecurityDescriptor;
+        public int bInheritHandle;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct JobobjectExtendedLimitInformation
+    {
+        public JobobjectBasicLimitInformation BasicLimitInformation;
+        public IoCounters IoInfo;
+        public UIntPtr ProcessMemoryLimit;
+        public UIntPtr JobMemoryLimit;
+        public UIntPtr PeakProcessMemoryUsed;
+        public UIntPtr PeakJobMemoryUsed;
+    }
+
+    public enum JobObjectInfoType
+    {
+        AssociateCompletionPortInformation = 7,
+        BasicLimitInformation = 2,
+        BasicUiRestrictions = 4,
+        EndOfJobTimeInformation = 6,
+        ExtendedLimitInformation = 9,
+        SecurityLimitInformation = 5,
+        GroupInformation = 11
+    }
+
+    public enum LimitFlags
+    {
+        JobObjectLimitKillOnJobClose = 0x2000,
+    }
+
+    #endregion
+
+}
---
+++ Terminator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Terminator
+{
+    internal class Program
+    {
+        private static List<string> PrettyPleaseKill(List<Process> processes)
+        {
+            var errors = new List<string>();
+            processes.ForEach(_ =>
+            {
+                try
+                {
+                    _.Kill();
+                }
+                catch (Exception ex)
+                {
+                    errors.Add($"{_.Id}\t{_.ProcessName}\t{ex.Message}");
+                }
+            });
+            return errors;
+        }
+
+        private static List<string> JustKillAlready(List<Process> processes)
+        {
+            var job = new Job();
+            job.AddProcess(processes.Select(_ => _.Handle));
+            job.Dispose();
+            return new List<string>();
+        }
+
+
+        private static void Main(string[] args)
+        {
+            var patternFileName = $"{Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName)}.txt";
+            var pattern = string.Join("|", File.ReadAllLines(patternFileName));
+
+            var stopWatchToDiscover = new Stopwatch();
+            var selectedProcesses = Process.GetProcesses()
+                .Where(_ => Regex.IsMatch(_.ProcessName, pattern, RegexOptions.IgnoreCase))
+                .OrderBy(_ => _.ProcessName)
+                .ToList();
+            stopWatchToDiscover.Stop();
+
+            var colorBefore = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(string.Join(Environment.NewLine, selectedProcesses.Select(_ => $"{_.Id}\t{_.ProcessName}\t{(DateTime.Now - _.StartTime).TotalSeconds}s")));
+            Console.ForegroundColor = colorBefore;
+
+            List<string> errors;
+            var stopWatchToKill = new Stopwatch();
+            if (args.Any() && args[0] == "PrettyPlease")
+                errors = PrettyPleaseKill(selectedProcesses);
+            else
+                errors = JustKillAlready(selectedProcesses);
+            stopWatchToKill.Stop();
+
+            if(stopWatchToDiscover.Elapsed.Milliseconds != 0)
+                Console.WriteLine($"Time to discover {stopWatchToDiscover.Elapsed.TotalMilliseconds}");
+
+            if(stopWatchToKill.Elapsed.Milliseconds != 0)
+                Console.WriteLine($"Time to kill {stopWatchToKill.Elapsed.TotalMilliseconds}");
+
+            if (selectedProcesses.Any())
+            {
+                Console.WriteLine($"{selectedProcesses.Count} process detected");
+                if (errors.Any())
+                {
+                    colorBefore = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(string.Join(Environment.NewLine, errors));
+                    Console.ForegroundColor = colorBefore;
+                }
+            }
+
+            var secondsToClose = args.Length >= 2 ? Convert.ToInt32(args[1]) : 1;
+            if (secondsToClose <= 0) return;
+
+            Console.WriteLine($"Press to quit or wait {secondsToClose} seconds");
+            Task.Factory.StartNew(Console.ReadKey).Wait(TimeSpan.FromSeconds(secondsToClose));
+        }
+    }
+}
